b1c404e45a5094ee02d6e0be3d6c0371619ad632
Piyush Goyani <thesourcepedia@gmail.com>
2023-03-13 15:14:29 +0530

    add fetch resolver and schema

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,15 +1,27 @@
+const fetch = require("node-fetch")
 const resolvers = {
     Query: {
         // returns an array of Tracks that will be used to populate the homepage grid of our web client
         tracksForHome: (_, __, { dataSources }) => {
             return dataSources.trackAPI.getTracksForHome();
         },
+        tracksForHomeFetch: async () => {
+            const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+            const res = await fetch(`${baseUrl}/tracks`);
+            return res.json();
+        }
     },
     Track: {
         // (parent, args, context, info)
-        author: ({ authorId }, _, { dataSources }) => {
-            return dataSources.trackAPI.getAuthor(authorId);
-        },
+        // author: ({ authorId }, _, { dataSources }) => {
+        //     return dataSources.trackAPI.getAuthor(authorId);
+        // },
+        // using fetch instead datasources
+        author: async ({ authorId }, _, { dataSources }) => {
+            const baseUrl = "https://odyssey-lift-off-rest-api.herokuapp.com";
+            const res = await fetch(`${baseUrl}/author/${authorId}`);
+            return res.json();
+        }
     },
 };
 
