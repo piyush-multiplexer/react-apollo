b1fc9fd2bf85de37ce34fa1e9e61457d255acf1b
Piyush Goyani <thesourcepedia@gmail.com>
2023-03-13 14:50:14 +0530

    graphQL realdata with resolver, datasource, api and schema in node server

--- server/src/schema.js
+++ server/src/schema.js
@@ -1,27 +1,34 @@
-const { gql } = require("apollo-server")
+const { gql } = require('apollo-server');
 
 const typeDefs = gql`
-    type Query {
-        "Get tracks array"
-        tracksForHome: [Track!]!
-    }
+  type Query {
+    "Query to get tracks array for the homepage grid"
+    tracksForHome: [Track!]!
+  }
 
-    " A track grounp of modules"
-    type Track {
-        id: ID!
-        title: String!
-        author: Author!
-        thumbnail: String
-        length: Int
-        modulesCount: Int
-    }
+  "A track is a group of Modules that teaches about a specific topic"
+  type Track {
+    id: ID!
+    "The track's title"
+    title: String!
+    "The track's main Author"
+    author: Author!
+    "The track's illustration to display in track card or track page detail"
+    thumbnail: String
+    "The track's approximate length to complete, in minutes"
+    length: Int
+    "The number of modules this track contains"
+    modulesCount: Int
+  }
 
-    "Author of Track"
-    type Author {
-        id: ID!
-        name: String!
-        photo: String
-    }
+  "Author of a complete Track or a Module"
+  type Author {
+    id: ID!
+    "Author's first and last name"
+    name: String!
+    "Author's profile picture"
+    photo: String
+  }
 `;
 
-module.exports = typeDefs
+module.exports = typeDefs;
